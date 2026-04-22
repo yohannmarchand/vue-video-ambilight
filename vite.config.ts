56045ea1e29717a23a5825d192f9a4fdf046a533
yohannmarchand <marchandyohannnnn@gmail.com>
2026-04-22 23:34:34 +0200

    fix: generate bundled type declarations

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import dts from 'vite-plugin-dts'
 import { resolve } from 'path'
 
 export default defineConfig({
-  plugins: [vue(), dts({ tsconfigPath: './tsconfig.app.json' })],
+  plugins: [vue(), dts({ tsconfigPath: './tsconfig.app.json', rollupTypes: true })],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
@@ -15,6 +15,7 @@ export default defineConfig({
       external: ['vue'],
       output: {
         globals: { vue: 'Vue' },
+        exports: 'named',
       },
     },
   },
